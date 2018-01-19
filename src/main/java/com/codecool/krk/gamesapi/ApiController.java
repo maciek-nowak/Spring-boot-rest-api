d856f88d4d624d7e0178a9e69c95a3d75ea66972
Maciej Nowak <mcnowak@wp.pl>
2018-01-19 01:29:09 +0100

    Implement logging to email address

--- src/main/java/com/codecool/krk/gamesapi/ApiController.java
+++ src/main/java/com/codecool/krk/gamesapi/ApiController.java
@@ -1,5 +1,6 @@
 package com.codecool.krk.gamesapi;
 
+import com.codecool.krk.gamesapi.logger.LoggerCreatorImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class ApiController {
     @GetMapping("")
     public String getDocumentation() {
         String link = "{\"api documentation\": \"https://github.com/CodecoolKRK20171/spring-boot-project-mcnowak-codecool\"}";
+        new LoggerCreatorImpl().logErrorEmailString(link);
         return link;
     }
 }
